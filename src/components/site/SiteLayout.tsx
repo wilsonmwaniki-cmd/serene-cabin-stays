3be9b6c17218e3b5e50361face113e6c2a1c8b6d
Mwaniki Will <Mwaniki1@Wilsons-MacBook-Pro.local>
2026-04-24 12:23:04 +0300

    Scroll to top on page navigation

--- src/components/site/SiteLayout.tsx
+++ src/components/site/SiteLayout.tsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { SiteHeader } from "./SiteHeader";
 import { SiteFooter } from "./SiteFooter";
 
 export const SiteLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [location.pathname]);
+
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <SiteHeader />
